5dcefffd91ba67a51abc7fcb1c79f99346e00a7b
Mathieu Gilliot <mathieu.gilliot.pro@gmail.com>
2021-06-02 15:26:52 +0200

    adding loader while downloading update

--- src/assets/js/updateNotification.js
+++ src/assets/js/updateNotification.js
@@ -4,17 +4,19 @@ const notification = document.getElementById('notification');
 const message = document.getElementById('message');
 const restartButton = document.getElementById('restart-button');
 const closeButton = document.getElementById("close-button");
-
+const loader = document.getElementById("loader");
 ipcRenderer.on('update_available', () => {
   ipcRenderer.removeAllListeners('update_available');
   message.innerText = 'Une nouvelle version du logiciel est disponible, téléchargement en court...';
   notification.classList.remove('hidden');
+  loader.classList.remove('hidden');
 });
 ipcRenderer.on('update_downloaded', () => {
   ipcRenderer.removeAllListeners('update_downloaded');
   message.innerText = "Mise à jour téléchargée, elle sera installée au redémarrage de l'application. Redémarrer maintenant?";
   restartButton.classList.remove('hidden');
   notification.classList.remove('hidden');
+  loader.classList.add('hidden');
 });
 
 function closeNotification() {
