206d9f6909a39be813758bc0245c4b454fd2f7e5
Mathieu Gilliot <mathieu.gilliot.pro@gmail.com>
2021-06-02 15:54:12 +0200

    icon

--- src/assets/js/updateNotification.js
+++ src/assets/js/updateNotification.js
@@ -5,6 +5,7 @@ const message = document.getElementById('message');
 const restartButton = document.getElementById('restart-button');
 const closeButton = document.getElementById("close-button");
 const loader = document.getElementById("loader");
+
 ipcRenderer.on('update_available', () => {
   ipcRenderer.removeAllListeners('update_available');
   message.innerText = 'Une nouvelle version du logiciel est disponible, téléchargement en court...';
