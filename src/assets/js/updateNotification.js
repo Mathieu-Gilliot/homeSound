4a3b4200a7408a56fa4f05fc8e09823be080ab25
Mathieu Gilliot <mathieu.gilliot.pro@gmail.com>
2021-06-02 14:20:09 +0200

    styling update notification

--- src/assets/js/updateNotification.js
+++ src/assets/js/updateNotification.js
@@ -6,12 +6,12 @@ const restartButton = document.getElementById('restart-button');
 
 ipcRenderer.on('update_available', () => {
   ipcRenderer.removeAllListeners('update_available');
-  message.innerText = 'A new update is available. Downloading now...';
+  message.innerText = 'Une nouvelle version du logiciel est disponible, téléchargement en court...';
   notification.classList.remove('hidden');
 });
 ipcRenderer.on('update_downloaded', () => {
   ipcRenderer.removeAllListeners('update_downloaded');
-  message.innerText = 'Update Downloaded. It will be installed on restart. Restart now?';
+  message.innerText = "Mise à jour téléchargée, elle sera installée au redémarrage de l'application. Redémarrer maintenant?";
   restartButton.classList.remove('hidden');
   notification.classList.remove('hidden');
 });
